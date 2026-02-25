407183acbfb6ed9f6af06db4e6a27f7220c7e89c
Kainan :] <cte-kainan.smith@sllboces.org>
2026-02-25 17:35:45 +0000

    Travel WIP, make Inventory next

--- travel.py
+++ travel.py
@@ -1,17 +1,31 @@
-newPlace = False
 currentPlace = 1
 visitedPlaces = []
+availablePlaces = []
 
 def go_to_new_place(place):
     # place variable is where you currently are
+    # Use the "availablePlaces" list to try and condense this code into less lines.
+    newPlace = False
     while newPlace == False:
-        if currentPlace == 1:
+        if place == 1:
             playerSelection = int(input("Where would you like to go? (2, 4, or 0[Dev Room])"))
             if playerSelection == 2:
                 if 2 in visitedPlaces:
                     yesOrNo = input("You've already been to 2, go back anyways? (Type \"yes\" or \"no\")")
-                    if yesOrNo == "yes":
-                        print("Going to 2")
-                    else:
+                    if yesOrNo == "no":
                         continue
-                currentPlace = 2
+                print("Going to 2")
+                place = 2
+                newPlace = True
+            elif playerSelection == 4:
+                if 4 in visitedPlaces:
+                    yesOrNo = input("You've already been to 2, go back anyways? (Type \"yes\" or \"no\")")
+                    if yesOrNo == "no":
+                        continue
+                print("Going to 2")
+                place = 4
+                newPlace = True
+        return place
+
+currentPlace = go_to_new_place(currentPlace)
+print(currentPlace)
