7413cf8408b1442108e130826034335a401ba90f
Kainan :] <cte-kainan.smith@sllboces.org>
2026-03-02 20:02:15 +0000

    Started character.py

---
+++ character.py
@@ -0,0 +1,12 @@
+import inventory
+# TODO: Figure out Defense Modifier equation
+charStats = {
+    "Level": 1,
+    "Health": 100,
+    "Mana": 100,
+    "Damage Modifier": 1,
+    "Defense Modifier": 1,
+    # Defense Modifier is from 0 to 100.
+    # damageTaken = (incomingDamage * )
+
+}
