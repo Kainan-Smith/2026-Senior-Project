864f8cc0fd09dcc828bacb0a7306e787a22ac603
Kainan :] <cte-kainan.smith@sllboces.org>
2026-04-27 14:52:10 +0000

    help me

--- combat.py
+++ combat.py
@@ -2,7 +2,8 @@ import character
 import items
 import inventory
 
-playerHealth = character.stats["Health"]
+playerTotal = character.stats["Health"]
+playerHealth = playerTotal
 enemyTotal = 100
 enemyHealth = enemyTotal
 userAction = ""
@@ -10,20 +11,29 @@ userAction = ""
 def displayHealth(total, current):
     unit = total / 25
     checking = 0
+    print(f"{int(current)}/{total}")
     print("[", end="")
     while checking < total:
         if checking <= current:
-            print("|", end="")
+            print("I", end="")
         else:
             print("-", end="")
         checking += unit
     print("]")
 
+def enemyTurn(player):
+    damage = 30
+    print("Enemy Attacks Player for", damage, " damage!")
+    return damage
+
 while playerHealth > 0 and enemyHealth > 0 and userAction != "Q":
+    print("Player: ", end="")
+    displayHealth(playerTotal, playerHealth)
+    print("Enemy: ", end="")
     displayHealth(enemyTotal, enemyHealth)
     guarding = False
     print("What will you do?")
-    userAction = input("Attack\nSpell\nGuard\nItems\n").title()
+    userAction = input(f"{"Attack":<12}{"Spell":<12}\n{"Guard":<12}{"Items"}\n").title()
     if userAction == "Attack":
         userDmg = (inventory.currentWeapon.number * character.stats["Physical Attack Mod."])
         enemyHealth -= userDmg
@@ -33,3 +43,4 @@ while playerHealth > 0 and enemyHealth > 0 and userAction != "Q":
         guarding = True
     elif userAction == "Items":
         pass
+    playerHealth -= enemyTurn(playerHealth)
