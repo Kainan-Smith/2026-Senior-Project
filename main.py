f8cb255472c1b8310a9855692a4dd3b9c7823682
Kainan :] <cte-kainan.smith@sllboces.org>
2026-05-19 14:42:08 +0000

    god

---
+++ main.py
@@ -0,0 +1,14 @@
+import character
+import combat
+import evil
+import inventory
+import items
+import newWorld
+import shopNPCs
+import spells
+import test
+import texts
+import travel
+
+if __name__ == "__main__":
+    
---
+++ newWorld.py
@@ -0,0 +1,11 @@
+import items
+
+class World:
+    def __init__(self):
+        self.name = ""
+        self.desc = ""
+        self.shop = items.allItems
+        self.shopName = ""
+
+def arrive(world):
+    
